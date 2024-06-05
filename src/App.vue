80c248bfcc1f42c8d00e5345238203f063ea7376
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-06-05 22:03:32 +0200

    [UPD] Milestone 1: completed

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,57 @@
 <script>
 import SearchMovies from './components/SearchMovies.vue';
+import axios from 'axios';
+import { store } from './store'
+
+
 
 export default {
   name: 'App',
   components: {
-    SearchMovies
+    SearchMovies,
+  },
+  data() {
+    return {
+      store,
+    }
+  },
+  methods: {
+    onSearch(inputText) {
+      console.log(inputText)
+      console.log(store.results)
+
+
+      axios.get(`https://api.themoviedb.org/3/search/movie?api_key=8a3b83078838507514062cfc870ff804&query=${inputText}&language=it-IT`).then((response) => {
+        this.store.results = response.data.results;
+
+      });
+
+      const filteredResults = store.results.filter((result) =>
+        result.original_title.toLowerCase().includes(inputText.toLowerCase()) || result.title.toLowerCase().includes(inputText.toLowerCase()) || result.original_language.toLowerCase().includes(inputText.toLowerCase()) || result.original_language.toLowerCase().includes(inputText.toLowerCase()) || result.vote_average.toString().includes(inputText))
+      console.log(filteredResults);
+      store.results = filteredResults;
+    },
+  },
+  created() {
+
   }
 };
+
+
 </script>
 
 <template>
   <div id="app">
-    <SearchMovies />
+    <SearchMovies @search-event="onSearch" />
   </div>
 </template>
 
 
 
-<style>
+<style lang="scss">
+@use './assets/scss/main.scss' as *;
+
 body {
-  background-color: #f8f9fa;
+  background-color: '$body-bg-color';
 }
 </style>
--- src/components/SearchMovies.vue
+++ src/components/SearchMovies.vue
@@ -1,6 +1,15 @@
 <script>
+import { store } from '../store'
 export default {
   name: 'SearchMovies',
+  data(){
+    return {
+      store,
+       inputText: '',
+    }
+  }
+  
+  
 
 }
 </script>
@@ -9,10 +18,22 @@ export default {
   <div class="container mt-5">
     <div class="row">
       <div class="col-md-6 offset-md-3">
-        <h1 class="text-center">Cerca Film</h1>
+        <h1 class="text-center text-danger">Cerca Film</h1>
         <div class="input-group mb-3">
-          <input type="text" class="form-control" placeholder="Inserisci il nome del film"/>
-          <button class="btn btn-primary">Cerca</button>
+          <input v-model="inputText" type="text" class="form-control" placeholder="Inserisci il nome del film o serie" @keyup.enter="$emit('search-event', inputText)" />
+          <button @click="$emit('search-event', inputText)" class="btn btn-primary">Cerca</button>
+        </div>
+        <!-- Mostra i risultati -->
+        <div v-if="store.results.length > 0" class="mt-3">
+          <h2 class="text-center text-white">Risultati della ricerca:</h2>
+          <div v-for="film in store.results" :key="film.id" class="card mb-3">
+           <div class="card-body">
+            <h5 class="card-title">{{ film.title }}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">{{ film.original_title }}</h6>
+            <p class="card-text">Lingua: {{ film.original_language }}</p>
+              <p class="card-text">Voto: {{ film.vote_average }}</p>
+           </div>
+          </div>
         </div>
       </div>
     </div>
