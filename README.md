8ee28c288896188b06236c1dd9475d5f548df24c
Vincenzo Fabrizio Viola <104321586+violix96@users.noreply.github.com>
2024-06-05 13:16:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Template Vite Vue
+
+Template di partenza per progetti Vite Vue
+
+## Installazione
+
+```bash
+  npm install
+  npm run dev
+```
---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<!-- <script></script> -->
+<template>
+  <h1>Vue Vite Template</h1>
+  <p>Template di partenza per progetti Vite Vue</p>
+</template>
+<!-- <style></style> -->
